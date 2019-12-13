444b86dbf006e8f58c0bf6abc9f396033bb3fa65
akbel <colakakbel@gmail.com>
2019-12-13 23:31:06 +0100

    1th try weakness

--- functions/find-by/weakness/index.js
+++ functions/find-by/weakness/index.js
@@ -1,5 +1,17 @@
 const weakness = (pokeArray, type) => {
+    //if (pokeArray.length !== 0){
+        let myFilter= pokeArray.filter(i => i.weakness === type);
+        console.log(myFilter);
 
+        /*
+        let myMap = myFilter.map(e => {
+            return { name: e.name, num: e.num };
+          });
+          //return myMap;}
+          console.log (myMap);}
+          else {return [];}
+
+        */
 };
 
 module.exports = weakness;
