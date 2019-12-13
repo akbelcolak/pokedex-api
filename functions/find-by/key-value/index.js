59c7447375bb8b4771f55ad657eacdf5d63c800e
akbel <colakakbel@gmail.com>
2019-12-13 18:41:18 +0100

    added code to find by key/value

--- functions/find-by/key-value/index.js
+++ functions/find-by/key-value/index.js
@@ -3,7 +3,18 @@
 // (later you will learn about req.body to pass any type)
 
 const keyValue = (pokeArray, key, value) => {
-
+    if(pokeArray.length !== 0){
+        // I filter pokemons with given key and value
+        let myFilter = pokeArray.filter(i => i[key] == value);
+      
+        // i return each filterd pokemon with their name and number data
+        let myMap = myFilter.map( e => {
+             return {'name': e.name, 'num': e.num};
+            
+          });
+          return myMap;}
+          else {return [];}
+      };
 };
 
 
