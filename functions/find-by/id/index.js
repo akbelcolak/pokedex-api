fcf1eabb63a17c1d92fe678a389592a714001d49
akbel <colakakbel@gmail.com>
2019-12-13 18:39:00 +0100

    added code to find by id

--- functions/find-by/id/index.js
+++ functions/find-by/id/index.js
@@ -1,5 +1,7 @@
 const id = (pokeData, id) => {
-
+    if (pokeData.find(pokemon => pokemon.id === id)) {
+        return pokeData.find(pokemon => pokemon.id === id);
+    } else { return {};}
 }
 
 module.exports = id;
