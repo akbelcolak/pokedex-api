a2953dbd0d9d422fbb67ada0e7c15199747d40a5
akbel <colakakbel@gmail.com>
2019-12-13 20:14:56 +0100

    added code to find by type

--- functions/find-by/type/index.js
+++ functions/find-by/type/index.js
@@ -1,5 +1,18 @@
 const type = (pokeArray, type) => {
+  if (pokeArray.length !== 0){
+  // I filtered the type, but this one gives me only exmp. the type 'dragon' , if poke has also other types, funk does not give me the name of poke.
+  // let myFilter= pokeArray.filter(pokemon => pokemon.type == type);
+  let myFilter= pokeArray.filter(pokemon => pokemon.type.includes(type) );
+  //console.log(myFilter);
 
+  // I return each filterd pokemon with their name and number data
+  let myMap = myFilter.map(e => {
+    return { name: e.name, num: e.num };
+  });
+  return myMap;}
+  //console.log (myMap);}
+  else {return [];}
+  /*  */
 };
 
 module.exports = type;
